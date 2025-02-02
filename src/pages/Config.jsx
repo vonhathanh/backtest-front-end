38023b0c4946712eb1f4b416840d051ec5b35ada
hanhvn <vonhathanh@gmail.com>
2025-02-02 17:14:36 +0700

    implement basic layout for configuration page

--- src/pages/Config.jsx
+++ src/pages/Config.jsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router";
+import "./Config.css"
+
 
 export default function Config() {
     const navigate = useNavigate();
@@ -8,6 +10,26 @@ export default function Config() {
     }
     
     return (
-        <button onClick={handleRedirect}>Start</button>
+      <>
+        <h3>Trading Strategy Configuration</h3>
+
+        <section id="data-selection">
+        </section>
+
+        <section id="symbol-and-timeframe" className="config-group">
+          <section className="config-value"></section>
+          <section className="config-value"></section>
+        </section>
+
+        <section id="time-and-balance" className="config-group">
+          <section className="config-value"></section>
+          <section className="config-value"></section>
+        </section>
+
+        <section id="manual-mode"></section>
+        <section id="strategy-selection"></section>
+
+        <button onClick={handleRedirect}>Start testing</button>
+      </>
     )
 }
