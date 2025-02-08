46bbf087fb5d58d445af41cc8ba084fd570a9637
hanhvn <vonhathanh@gmail.com>
2025-02-08 09:04:51 +0700

    use prop.text instead of prop.name in Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,10 @@
 export default function Button(props) {
-
+    
     const style = props.isSelected ? { backgroundColor: "black", color: "white" } : {}
+
     return (
         <>
-            <button className="max-width-btn" style={style} onClick={props.onClick}>{props.name}</button>
+            <button className="max-width-btn" style={style} onClick={props.onClick}>{props.text}</button>
         </>
     )
 }
