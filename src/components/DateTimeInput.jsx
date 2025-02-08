42ea3b8eae7eff4ab8f5e439cfa29753ad72eb89
hanhvn <vonhathanh@gmail.com>
2025-02-08 07:29:01 +0700

    remove id in Input & DateTimeInput

--- src/components/DateTimeInput.jsx
+++ src/components/DateTimeInput.jsx
@@ -2,7 +2,7 @@ export default function DateTimeInput({label, isEnable, value}) {
     return (
         <section>
             <label htmlFor={label}>{label}</label><br/>
-            <input type="date" id={label} disabled={isEnable ? "" : "disabled"} value={value} />
+            <input type="date" disabled={isEnable ? "" : "disabled"} value={value} />
         </section>
     )
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,7 @@ export default function Input({label, isEnable, value}) {
     return (
         <section>
             <label htmlFor={label}>{label}</label><br/>
-            <input type="text" id={label} disabled={isEnable ? "" : "disabled"} value={value} />
+            <input type="text" disabled={isEnable ? "" : "disabled"} value={value} />
         </section>
     )
 }
