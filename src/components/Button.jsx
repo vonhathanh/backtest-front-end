847f5fc0d63881a27286c868151fae34ef64ca3d
hanhvn <vonhathanh@gmail.com>
2025-02-08 07:29:15 +0700

    add common Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+export default function Button(props) {
+
+    const style = props.isSelected ? { backgroundColor: "black", color: "white" } : {}
+    return (
+        <>
+            <button className="max-width-btn" style={style} onClick={props.onClick}>{props.name}</button>
+        </>
+    )
+}
