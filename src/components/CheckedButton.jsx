b1eacb3f6814228ac3db3d86e7c062acd195cdba
hanhvn <vonhathanh@gmail.com>
2025-02-08 10:35:10 +0700

    add button with checked icon at the end

---
+++ src/components/CheckedButton.jsx
@@ -0,0 +1,13 @@
+export default function CheckedButton(props) {
+    
+    const style = props.isSelected ? { backgroundColor: "black", color: "white" } : {}
+
+    return (
+        <>
+            <button className="max-width-btn flex" style={style} onClick={props.onClick}>
+                {props.text}
+                {props.isSelected && <i className="fa-solid fa-check checked-icon"></i>}
+            </button>
+        </>
+    )
+}
