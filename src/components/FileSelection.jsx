05416fb7354794929f1b48b6b16bb37c0fc19aee
hanhvn <vonhathanh@gmail.com>
2025-02-10 11:24:02 +0700

    add load file logics to FileSelection component

--- src/components/FileSelection.jsx
+++ src/components/FileSelection.jsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react"
+import { fetchFilenames } from "../api"
+import Loader from "./Loader"
+import ErrorMessage from "./ErrorMessage"
+
 export default function FileSelection() {
-    return (
-        <section id="data-selection">
-            <span>Choose data file</span>
-            <button id="upload-btn">
-                <i id="upload-icon" className="fa-solid fa-arrow-up-from-bracket icon"></i>
-                Upload File
-            </button>
-        </section>
-    )
+
+    const [filenames, setFilenames] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        fetchFilenames()
+            .then(setFilenames)
+            .catch(setError)
+            .finally(() => {
+                setLoading(false)
+            })
+    }, [])
+
+    function render() {
+        if (loading) return <Loader />
+        if (error) return <ErrorMessage message={`Can't load strategies, reason: ${error.message}`} />
+        return (
+            <section id="data-selection">
+                <span>Choose data file</span>
+                <button id="upload-btn">
+                    <i id="upload-icon" className="fa-solid fa-arrow-up-from-bracket icon"></i>
+                    Upload File
+                </button>
+            </section>
+        )
+    }
+
+    return render()
+
 }
