bd2b35bc5a7398954e705b7a25507b66dceabd21
hanhvn <vonhathanh@gmail.com>
2025-02-10 11:08:24 +0700

    css: add icon class

--- src/components/FileSelection.jsx
+++ src/components/FileSelection.jsx
@@ -3,7 +3,7 @@ export default function FileSelection() {
         <section id="data-selection">
             <span>Choose data file</span>
             <button id="upload-btn">
-                <i id="upload-icon" className="fa-solid fa-arrow-up-from-bracket"></i>
+                <i id="upload-icon" className="fa-solid fa-arrow-up-from-bracket icon"></i>
                 Upload File
             </button>
         </section>
