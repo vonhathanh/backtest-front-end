9b74834003f9d15209c1dcda41634fb6190d9b94
hanhvn <vonhathanh@gmail.com>
2025-02-12 09:39:23 +0700

    conditional render DateTimeInput on config data

--- src/components/DateTimeInput.jsx
+++ src/components/DateTimeInput.jsx
@@ -1,8 +1,30 @@
-export default function DateTimeInput({label, isEnable, value}) {
+export default function DateTimeInput({ label, config }) {
+    function render() {
+        if (config) {
+            return (
+                <input
+                    type="date"
+                    disabled=""
+                    value={label.includes("Start") ? config.start_time : config.end_time}
+                    min={config.start_time}
+                    max={config.end_time}
+                />
+            )
+        } else {
+            return (
+                <input
+                    type="date"
+                    disabled="disabled"
+                    value=""
+                />
+            )
+        }
+    }
+
     return (
         <section>
-            <label htmlFor={label}>{label}</label><br/>
-            <input type="date" disabled={isEnable ? "" : "disabled"} value={value} />
+            <label htmlFor={label}>{label}</label><br />
+            {render()}
         </section>
     )
 }
