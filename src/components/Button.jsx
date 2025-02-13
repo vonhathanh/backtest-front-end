c650b778c189e000d4d498605855c736d4e3416b
hanhvn <vonhathanh@gmail.com>
2025-02-13 16:54:49 +0700

    refactor Button: add type="button"

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,8 +3,6 @@ export default function Button(props) {
     const style = props.isSelected ? { backgroundColor: "black", color: "white" } : {}
 
     return (
-        <>
-            <button className="max-width-btn" style={style} onClick={props.onClick}>{props.text}</button>
-        </>
+        <button type="button" className="max-width-btn" style={style} onClick={props.onClick}>{props.text}</button>
     )
 }
