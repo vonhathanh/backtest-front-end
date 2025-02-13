43c3fdb2b0ea6956100681c85c7204474fa54da6
hanhvn <vonhathanh@gmail.com>
2025-02-13 16:55:18 +0700

    simplify DateTimeInput

--- src/components/DateTimeInput.jsx
+++ src/components/DateTimeInput.jsx
@@ -1,30 +1,21 @@
-export default function DateTimeInput({ label, config }) {
-    function render() {
-        if (config) {
-            return (
-                <input
-                    type="date"
-                    disabled=""
-                    value={label.includes("Start") ? config.start_time : config.end_time}
-                    min={config.start_time}
-                    max={config.end_time}
-                />
-            )
-        } else {
-            return (
-                <input
-                    type="date"
-                    disabled="disabled"
-                    value=""
-                />
-            )
-        }
+export default function DateTimeInput({ label, config, onChange }) {
+    function getValue() {
+        if (!config) return ""
+        return label.includes("Start") ? config.start_time : config.end_time
     }
 
     return (
         <section>
             <label htmlFor={label}>{label}</label><br />
-            {render()}
+            <input
+                type="date"
+                disabled={config ? "" : "disabled"}
+                name={label}
+                value={getValue()}
+                min={config?.start_time || ""}
+                max={config?.end_time || ""}
+                onChange={(e) => onChange(label, e.target.value)}
+            />
         </section>
     )
 }
