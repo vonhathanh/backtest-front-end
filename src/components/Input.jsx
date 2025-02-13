135d38cae9c569fd74674a5e8049f2849e5eb94a
hanhvn <vonhathanh@gmail.com>
2025-02-13 16:53:51 +0700

    add name for Input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,7 @@ export default function Input({label, isEnable, value, type}) {
     return (
         <section>
             <label htmlFor={label}>{label}</label><br/>
-            <input type={type} disabled={isEnable ? "" : "disabled"} value={value} />
+            <input type={type} name={label} disabled={isEnable ? "" : "disabled"} value={value} />
         </section>
     )
 }
