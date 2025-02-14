980feef3c52aba7a2758a1a959611b7acfd92785
hanhvn <vonhathanh@gmail.com>
2025-02-14 10:29:13 +0700

    add onChange event to Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,18 @@
-export default function Input({label, value, type}) {
+import { useContext } from "react"
+import { StrategyContext } from "../pages/Config"
+
+export default function Input({ label, value, type, parent }) {
+
+    const dispatch = useContext(StrategyContext)
+
     return (
         <section>
-            <label htmlFor={label}>{label}</label><br/>
-            <input type={type} name={label} value={value} />
+            <label htmlFor={label}>{label}</label><br />
+            <input 
+            type={type} 
+            name={label} 
+            value={value} 
+            onChange={parent ? (event) => dispatch({ type: "updated", id: parent, name: label, value: event.target.value }) : undefined} />
         </section>
     )
 }
