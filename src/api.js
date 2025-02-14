a278335bd0363add914f1e512203e1480ee184d1
hanhvn <vonhathanh@gmail.com>
2025-02-14 13:32:58 +0700

    refactor api calls

--- src/api.js
+++ src/api.js
@@ -1,19 +1,18 @@
 import axios from "axios";
 import config from "./config";
 
-// needed because FastAPI server require "Access-Control-Allow-Origin": "*" to be sent in the request
-const headers = { "Access-Control-Allow-Origin": "*" };
-
-export async function fetchStrategies() {
-    const response = await axios.get(`${config.apiUrl}/strategies`, {
-        headers: headers,
+export async function fetch(url) {
+    // header is needed because FastAPI server require "Access-Control-Allow-Origin": "*" to be sent in the request
+    const response = await axios.get(`${config.apiUrl}/${url}`, {
+        headers: { "Access-Control-Allow-Origin": "*" },
     });
     return response.data;
 }
 
+export async function fetchStrategies() {
+    return await fetch("strategies")
+}
+
 export async function fetchFilesMetadata() {
-    const response = await axios.get(`${config.apiUrl}/files/metadata`, {
-        headers: headers,
-    });
-    return response.data;
+    return await fetch("files/metadata")
 }
