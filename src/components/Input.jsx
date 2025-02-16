bf8c558f4e36fb3749ad1813d4f0853bd786032a
hanhvn <vonhathanh@gmail.com>
2025-02-17 05:54:08 +0700

    reduce font size for inputs of StrategyConfig component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,13 +5,15 @@ export default function Input({ label, value, type, parent }) {
 
     const dispatch = useContext(StrategyContext)
 
+    const style = parent ? {fontSize: "0.875rem"} : {fontSize: "1rem"}
+
     return (
         <section>
-            <label htmlFor={label}>{label}</label><br />
+            <label style={style} htmlFor={label}>{label}</label><br />
             <input 
             type={type} 
             name={label} 
-            value={value} 
+            value={value}
             onChange={parent ? (event) => dispatch({ type: "updated", strategyId: parent, name: label, value: event.target.value }) : undefined} />
         </section>
     )
