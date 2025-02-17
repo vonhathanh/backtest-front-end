0d5b9207150f5e76a0042a6977af8d56095936d4
hanhvn <vonhathanh@gmail.com>
2025-02-17 14:43:14 +0700

    implement Backtest component

--- src/pages/Backtest.jsx
+++ src/pages/Backtest.jsx
@@ -1,22 +1,34 @@
 import { useLocation } from "react-router-dom"
 
+import "./Backtest.css"
+import ErrorMessage from "../components/ErrorMessage"
+import CandleStickChart from "../components/CandleStickChart"
+import { useEffect } from "react"
+
 export default function Backtest() {
     const location = useLocation()
     const backtestParams = location.state
+
+    if (!backtestParams) {
+        return <ErrorMessage message="Backtest parameters are not valid/null"/>
+    }
+
+    useEffect(() => {
+        
+    })
     
     return (
-        <main>
+        <main className="backtest">
             <section className="chart-container">
-                <section className="chart-information">Chart Infomation</section>
-                <section className="chart">Chart</section>
+                <CandleStickChart symbol={backtestParams.symbol} tf={backtestParams.tf}/>
             </section>
 
-            <section>
+            {/* <section>
                 Account Info
                 <section>Positions</section>
                 <section>Open orders</section>
                 <section>Order history</section>
-            </section>
+            </section> */}
             
         </main>
     )
