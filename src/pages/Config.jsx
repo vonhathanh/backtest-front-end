453a17f990103e3adac6b48deb1841e5db40dcbd
hanhvn <vonhathanh@gmail.com>
2025-03-04 20:31:36 +0700

    remove logs

--- src/pages/Config.jsx
+++ src/pages/Config.jsx
@@ -34,8 +34,6 @@ export default function Config() {
       },
       strategies: Object.entries(strategies),
     };
-    console.log(backtestParams);
-
     navigate("/backtest", { state: backtestParams });
   }
 
