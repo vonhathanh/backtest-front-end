c47a0fc00e777542f7fd6b9adc804b24f7be130c
hanhvn <vonhathanh@gmail.com>
2025-03-09 07:45:59 +0700

    reformat PositionTable

--- src/components/PositionsTable.jsx
+++ src/components/PositionsTable.jsx
@@ -1,35 +1,35 @@
-export default function PositionsTable({positions, price}) {
-    const rows = positions.map((position, index) => {
-        let pnl = 0.0
-        if (price) {
-            pnl = Number(position.quantity * (price - position.averagePrice)).toFixed(2)
-            if (position.side === 'SHORT') pnl *= -1
-        }
-        return (
-            <tr key={index}>
-                <td>{position.side}</td>
-                <td>{position.quantity.toFixed(2)}</td>
-                <td>{position.averagePrice.toFixed(2)}</td>
-                <td>{pnl}</td>
-                <td>Close</td>
-            </tr>
-        )
-    })
-    
+export default function PositionsTable({ positions, price }) {
+  const rows = positions.map((position, index) => {
+    let pnl = 0.0;
+    if (price) {
+      pnl = Number(position.quantity * (price - position.averagePrice)).toFixed(
+        2
+      );
+      if (position.side === "SHORT") pnl *= -1;
+    }
     return (
-        <table id="positions-table">
-            <thead>
-                <tr>
-                    <th>Side</th>
-                    <th>Quantity</th>
-                    <th>Average Price</th>
-                    <th>PNL</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {rows}
-            </tbody>
-        </table>
-    )
+      <tr key={index}>
+        <td>{position.side}</td>
+        <td>{position.quantity.toFixed(2)}</td>
+        <td>{position.averagePrice.toFixed(2)}</td>
+        <td>{pnl}</td>
+        <td>Close</td>
+      </tr>
+    );
+  });
+
+  return (
+    <table id="positions-table">
+      <thead>
+        <tr>
+          <th>Side</th>
+          <th>Quantity</th>
+          <th>Average Price</th>
+          <th>PNL</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
 }
