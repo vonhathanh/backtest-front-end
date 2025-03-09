11abd0be3e25ba961c46c025969cbff5b5f1abaa
hanhvn <vonhathanh@gmail.com>
2025-03-09 21:14:35 +0700

    refactor codes

--- src/components/CandlestickChart.jsx
+++ src/components/CandlestickChart.jsx
@@ -12,19 +12,11 @@ export default function CandlestickChart({
   const chartContainerRef = useRef(null);
   const chartInstanceRef = useRef(null);
 
-  useEffect(renderChart, [params]);
+  useEffect(initChart, [params]);
   useEffect(drawOpenOrders, [openOrders]);
+  useEffect(updateChart, [params.delay, prices, socket]);
 
-  useEffect(() => {
-    chartInstanceRef.current.updateSeries([
-      { name: "candlestick", data: prices },
-    ]);
-    setTimeout(() => {
-      if (socket) socket.emit("render_finished", {});
-    }, params.delay * 1000);
-  }, [params.delay, prices, socket]);
-
-  function renderChart() {
+  function initChart() {
     // chart only need to render the first time, the rest is update series data
     if (chartContainerRef.current && !chartInstanceRef.current) {
       chartOptions.title.text = `${params.symbol} - ${params.timeframe}`;
@@ -47,6 +39,15 @@ export default function CandlestickChart({
         borderColor: order.side === "BUY" ? "#00E396" : "#F72411",
       });
     });
+  }
+
+  function updateChart() {
+    chartInstanceRef.current.updateSeries([
+      { name: "candlestick", data: prices },
+    ]);
+    setTimeout(() => {
+      if (socket) socket.emit("render_finished", {});
+    }, params.delay * 1000);
   }
 
   return <section id="candlestick-chart" ref={chartContainerRef}></section>;
