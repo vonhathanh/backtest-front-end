80ee02385e7dc68c68043d7d91e3d76b19beaef4
hanhvn <vonhathanh@gmail.com>
2025-03-15 15:53:46 +0700

    reformat code

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,33 +3,38 @@ import { StrategyContext } from "../pages/Config";
 import { toCamelCase } from "../utils";
 
 export default function Input({ label, value, type, parent, defaultValue }) {
-    const dispatch = useContext(StrategyContext);
+  const dispatch = useContext(StrategyContext);
 
-    // smaller font size for label in StrategyConfig
-    const style = parent ? {fontSize: "0.875rem"} : {fontSize: "1rem"}
+  // smaller font size for label in StrategyConfig
+  const style = parent ? { fontSize: "0.875rem" } : { fontSize: "1rem" };
 
-    return (
-        <section>
-            <label style={style} htmlFor={label}>{label}</label>
-            <br />
-            <input
-                type={type}
-                name={label}
-                value={value}
-                defaultValue={defaultValue? defaultValue : undefined}
-                required
-                onChange={
-                    parent
-                        ? (event) =>
-                              dispatch({
-                                  type: "updated",
-                                  strategyId: parent,
-                                  name: toCamelCase(label),
-                                  value: type === "number" ? Number(event.target.value) : event.target.value,
-                              })
-                        : undefined
-                }
-            />
-        </section>
-    );
+  return (
+    <section>
+      <label style={style} htmlFor={label}>
+        {label}
+      </label>
+      <br />
+      <input
+        type={type}
+        name={label}
+        value={value}
+        defaultValue={defaultValue ? defaultValue : undefined}
+        required
+        onChange={
+          parent
+            ? (event) =>
+                dispatch({
+                  type: "updated",
+                  strategyId: parent,
+                  name: toCamelCase(label),
+                  value:
+                    type === "number"
+                      ? Number(event.target.value)
+                      : event.target.value,
+                })
+            : undefined
+        }
+      />
+    </section>
+  );
 }
