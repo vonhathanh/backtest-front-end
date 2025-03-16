9df7493eadd39d55765e0d7988bc56d8c306e219
hanhvn <vonhathanh@gmail.com>
2025-03-16 11:34:53 +0700

    add ChartHeader component

---
+++ src/components/ChartHeader.jsx
@@ -0,0 +1,16 @@
+export default function ChartHeader({ params, socket }) {
+  return (
+    <section className="chart-header">
+      <span>
+        {params.symbol} - {params.timeframe}
+      </span>
+      <div className="next-btn-div">
+        {params.stepByStep ? (
+          <button className="next-btn" onClick={() => socket.emit("render_finished", {})}>
+            Next
+          </button>
+        ) : null}
+      </div>
+    </section>
+  );
+}
