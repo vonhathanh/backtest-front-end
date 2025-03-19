4a4f40efe3b41ae184f4a254f03cb069b6672c9f
hanhvn <vonhathanh@gmail.com>
2025-03-19 20:47:52 +0700

    remove symbol = order type

--- src/components/OrderHistoryTable.jsx
+++ src/components/OrderHistoryTable.jsx
@@ -2,9 +2,9 @@ export default function OrderHistoryTable({ orders }) {
   const rows = orders.map((order) => {
     return (
       <tr key={order.id}>
+        <td>{order.type}</td>
         <td>{order.side}</td>
         <td>{order.quantity.toFixed(2)}</td>
-        <td>{order.symbol}</td>
         <td>{order.price.toFixed(2)}</td>
         <td>{order.positionSide}</td>
         <td>{new Date(order.filledAt * 1000).toDateString()}</td>
@@ -17,9 +17,9 @@ export default function OrderHistoryTable({ orders }) {
       <table id="order-history-table" className="info-table">
         <thead>
           <tr>
+            <th>Type</th>
             <th>Side</th>
             <th>Quantity</th>
-            <th>Symbol</th>
             <th>Entry Price</th>
             <th>Position Side</th>
             <th>Filled at</th>
--- src/components/OrdersTable.jsx
+++ src/components/OrdersTable.jsx
@@ -2,9 +2,9 @@ export default function OrdersTable({ orders }) {
   const rows = orders.map((order) => {
     return (
       <tr key={order.id}>
+        <td>{order.type}</td>
         <td>{order.side}</td>
         <td>{order.quantity.toFixed(2)}</td>
-        <td>{order.symbol}</td>
         <td>{order.price.toFixed(2)}</td>
         <td>{order.positionSide}</td>
       </tr>
@@ -16,9 +16,9 @@ export default function OrdersTable({ orders }) {
       <table id="orders-table" className="info-table">
         <thead>
           <tr>
+            <th>Type</th>
             <th>Side</th>
             <th>Quantity</th>
-            <th>Symbol</th>
             <th>Entry Price</th>
             <th>Position Side</th>
           </tr>
