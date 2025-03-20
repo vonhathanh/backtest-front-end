7fb049d5b250a436b1417602f6ffe178ab3e95b0
hanhvn <vonhathanh@gmail.com>
2025-03-20 07:59:34 +0700

    fix next button emit incorrect event

--- src/components/ChartHeader.jsx
+++ src/components/ChartHeader.jsx
@@ -5,9 +5,7 @@ export default function ChartHeader({ params, socket, pnl }) {
         {params.symbol} - {params.timeframe}, PnL: {pnl.toFixed(2)}
       </span>
       <div className="next-btn-div">
-        {params.stepByStep ? (
-          <button onClick={() => socket.emit("render_finished", {})}>Next</button>
-        ) : null}
+        {params.stepByStep ? <button onClick={() => socket.emit("next", {})}>Next</button> : null}
       </div>
     </section>
   );
