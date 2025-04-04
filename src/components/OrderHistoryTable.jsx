5f6f5c5f776a7dd7b555f6d4e79fc76a8146f5c9
hanhvn <vonhathanh@gmail.com>
2025-04-05 06:59:10 +0700

    create Order component

--- src/components/OrderHistoryTable.jsx
+++ src/components/OrderHistoryTable.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
 import config from "../config";
+import Order from "./Order";
 
 export default function OrderHistoryTable({ orders }) {
   const [startIndex, setStartIndex] = useState(0);
@@ -21,24 +22,7 @@ export default function OrderHistoryTable({ orders }) {
   const bottomSpacerHeight = (orders.length - endIndex) * ROW_HEIGHT;
 
   const rows = visibleOrders.map((order) => {
-    const orderSideStyle = {
-      color: order.side == "Buy" ? "green" : "red",
-      fontWeight: "bold",
-    };
-    const positionSideStyle = {
-      color: order.positionSide == "Long" ? "green" : "red",
-      fontWeight: "bold",
-    };
-    return (
-      <tr key={order.id}>
-        <td style={{ fontWeight: "bold" }}>{order.type}</td>
-        <td style={orderSideStyle}>{order.side}</td>
-        <td>{order.quantity.toFixed(2)}</td>
-        <td>{order.price.toFixed(2)}</td>
-        <td style={positionSideStyle}>{order.positionSide}</td>
-        <td>{new Date(order.filledAt * 1000).toJSON().slice(0, -5)}</td>
-      </tr>
-    );
+    return <Order key={order.id} order={order} />;
   });
 
   return (
--- src/components/OrdersTable.jsx
+++ src/components/OrdersTable.jsx
@@ -1,22 +1,8 @@
+import Order from "./Order";
+
 export default function OrdersTable({ orders }) {
   const rows = orders.map((order) => {
-    const orderSideColor = {
-      color: order.side == "Buy" ? "green" : "red",
-      fontWeight: "bold",
-    };
-    const positionSideColor = {
-      color: order.positionSide == "Long" ? "green" : "red",
-      fontWeight: "bold",
-    };
-    return (
-      <tr key={order.id}>
-        <td style={{ fontWeight: "bold" }}>{order.type}</td>
-        <td style={orderSideColor}>{order.side}</td>
-        <td>{order.quantity.toFixed(2)}</td>
-        <td>{order.price.toFixed(2)}</td>
-        <td style={positionSideColor}>{order.positionSide}</td>
-      </tr>
-    );
+    return <Order key={order.id} order={order} />;
   });
 
   return (
