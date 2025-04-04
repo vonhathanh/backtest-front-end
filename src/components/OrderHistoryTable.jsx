627e4700e7965fcd3b4b451a042df850fdc8c0f1
hanhvn <vonhathanh@gmail.com>
2025-04-05 06:46:02 +0700

    refactor code

--- src/components/OrderHistoryTable.jsx
+++ src/components/OrderHistoryTable.jsx
@@ -21,21 +21,21 @@ export default function OrderHistoryTable({ orders }) {
   const bottomSpacerHeight = (orders.length - endIndex) * ROW_HEIGHT;
 
   const rows = visibleOrders.map((order) => {
-    const orderSideColor = {
+    const orderSideStyle = {
       color: order.side == "Buy" ? "green" : "red",
       fontWeight: "bold",
     };
-    const positionSideColor = {
+    const positionSideStyle = {
       color: order.positionSide == "Long" ? "green" : "red",
       fontWeight: "bold",
     };
     return (
       <tr key={order.id}>
         <td style={{ fontWeight: "bold" }}>{order.type}</td>
-        <td style={orderSideColor}>{order.side}</td>
+        <td style={orderSideStyle}>{order.side}</td>
         <td>{order.quantity.toFixed(2)}</td>
         <td>{order.price.toFixed(2)}</td>
-        <td style={positionSideColor}>{order.positionSide}</td>
+        <td style={positionSideStyle}>{order.positionSide}</td>
         <td>{new Date(order.filledAt * 1000).toJSON().slice(0, -5)}</td>
       </tr>
     );
